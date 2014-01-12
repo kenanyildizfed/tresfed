50686abaf04b5ba62e6a0794a4c50d6e2cf50024
umut doan <umutdoan@hotmail.com>
2014-01-12 17:29:48 +0200

    iletişim sayfası başlandı

---
+++ teknik-data/html/js/_custom-cufonreplace.js
@@ -0,0 +1,218 @@
+/*
+ *  Document   : cufonreplace.js
+ *  Author     : whmcsx
+ *  Description: The main cufon of the template
+ *	Cufon.replace('.normal', { fontFamily: 'name', hover: true  });
+ *	Cufon.replace('.normalitalic', { fontFamily: 'name', fontStyle:'italic', hover: true });
+ */
+
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Thin","font-weight":250,"
+=======================================================================================
+*/
+Cufon.replace('.rbtt', { fontFamily: 'Roboto Thin', hover: true, fontWeight: 250  });
+/*
+=======================================================================================
+TEKNIKDATA "face":{"font-family":"Roboto Thin","font-weight":250,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtti', { fontFamily: 'Roboto Thin', fontStyle:'italic', hover: true, fontWeight: 250 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Light","font-weight":300,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.rbtl, #footer .contact-area span.text,#footer .footer-bar p,.detaipagetitle h2 h3', { fontFamily: 'Roboto Light', hover: true, fontWeight: 300  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Light","font-weight":300,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtli', { fontFamily: 'Roboto Light', fontStyle:'italic', hover: true, fontWeight: 300 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto","font-weight":400,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.rbt, .motto p, #footer .contact-area p', { fontFamily: 'Roboto', hover: true, fontWeight: 400  });
+/*
+=======================================================================================
+TEKNIKDATA "face":{"font-family":"Roboto","font-weight":400,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbti', { fontFamily: 'Roboto', fontStyle:'italic', hover: true, fontWeight: 400 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Medium","font-weight":500,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.rbtm, .megamenu_drop,.pricingbox p,#footer .footer-bar p b', { fontFamily: 'Roboto Medium', hover: true, fontWeight: 500  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Medium","font-weight":500,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtmi', { fontFamily: 'Roboto Medium', fontStyle:'italic', hover: true, fontWeight: 500 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto","font-weight":700,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.rbtbl, .motto span,.allpricingbox span,.pricingbox span,.domainname span, #footer .contact-area b', { fontFamily: 'Roboto', hover: true, fontWeight: 700  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto","font-weight":700,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtbli', { fontFamily: 'Roboto', fontStyle:'italic', hover: true, fontWeight: 700 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Black","font-weight":900,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.rbtbk', { fontFamily: 'Roboto Black', hover: true, fontWeight: 900  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Black","font-weight":900,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtbki', { fontFamily: 'Roboto Black', fontStyle:'italic', hover: true, fontWeight: 900 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed Light","font-weight":300,"font-stretch":"condensed"
+=======================================================================================
+*/
+Cufon.replace('.rbtcl', { fontFamily: 'Roboto Condensed Light', hover: true, fontWeight: 300  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed Light","font-weight":300,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtcli', { fontFamily: 'Roboto Condensed Light', fontStyle:'italic', hover: true, fontWeight: 300 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed","font-weight":400,"font-stretch":"condensed"
+=======================================================================================
+*/
+Cufon.replace('.rbtc', { fontFamily: 'Roboto Condensed', hover: true, fontWeight: 400  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed","font-weight":400,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.rbtci', { fontFamily: 'Roboto Condensed', fontStyle:'italic', hover: true, fontWeight: 400 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed","font-weight":700,"font-stretch":"condensed"
+=======================================================================================
+*/
+Cufon.replace('.rbtb', { fontFamily: 'Roboto Condensed', hover: true, fontWeight: 700  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"Roboto Condensed","font-weight":700,"fontStyle":"italic",
+=======================================================================================
+*/
+Cufon.replace('.rbtbi', { fontFamily: 'Roboto Condensed', fontStyle:'italic', hover: true, fontWeight: 700 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro XThin","font-weight":100,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpxt', { fontFamily: 'PF BeauSans Pro XThin', hover: true, fontWeight: 100  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro XThin","font-weight":100,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpxti', { fontFamily: 'PF BeauSans Pro XThin', fontStyle:'italic', hover: true, fontWeight: 100 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Thin","font-weight":200,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpt', { fontFamily: 'PF BeauSans Pro Thin', hover: true, fontWeight: 200  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Thin","font-weight":200,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpti', { fontFamily: 'PF BeauSans Pro Thin', fontStyle:'italic', hover: true, fontWeight: 200 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Light","font-weight":300,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpl', { fontFamily: 'PF BeauSans Pro Light', hover: true, fontWeight: 300  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Light","font-weight":300,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpli', { fontFamily: 'PF BeauSans Pro Light', fontStyle:'italic', hover: true, fontWeight: 300 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro","font-weight":400,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbp', { fontFamily: 'PF BeauSans Pro', hover: true, fontWeight: 400  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro","font-weight":400,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpi', { fontFamily: 'PF BeauSans Pro', fontStyle:'italic', hover: true, fontWeight: 400 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Bbook","font-weight":500,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpb', { fontFamily: 'PF BeauSans Pro Bbook', hover: true, fontWeight: 500  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Bbook","font-weight":500,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpbi', { fontFamily: 'PF BeauSans Pro Bbook', fontStyle:'italic', hover: true, fontWeight: 500 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro SemiBold","font-weight":600,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpsb', { fontFamily: 'PF BeauSans Pro SemiBold', hover: true, fontWeight: 600  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro SemiBold","font-weight":600,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpsbi', { fontFamily: 'PF BeauSans Pro SemiBold', fontStyle:'italic', hover: true, fontWeight: 600 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro","font-weight":700,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpbc', { fontFamily: 'PF BeauSans Pro', hover: true, fontWeight: 700  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro","font-weight":700,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpbci', { fontFamily: 'PF BeauSans Pro', fontStyle:'italic', hover: true, fontWeight: 700 });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Black","font-weight":900,"font-stretch":"normal"
+=======================================================================================
+*/
+Cufon.replace('.pfbpblk', { fontFamily: 'PF BeauSans Pro Black', hover: true, fontWeight: 900  });
+/*
+=======================================================================================
+TEKNIKDATA "font-family":"PF BeauSans Pro Black","font-weight":900,"fontStyle":"italic"
+=======================================================================================
+*/
+Cufon.replace('.pfbpblki', { fontFamily: 'PF BeauSans Pro Black', fontStyle:'italic', hover: true, fontWeight: 900 });
+/*
+=======================================================================================
+TEKNIKDATA Final
+=======================================================================================
+*/
+
