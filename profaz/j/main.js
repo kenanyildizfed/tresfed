9c5b2332173c2792d71aba90e4ca8709ebdd5b42
umut doan <umutdoan@hotmail.com>
2014-01-12 21:20:31 +0200

    iletişim sayfası başlandı

---
+++ profaz/j/main.js
@@ -0,0 +1,32 @@
+PF = {
+
+    init: function() {
+        PF.General.init();
+    },
+
+    General: {
+
+        init: function(){
+            PF.General.FormValidator.init();
+console.log('a');
+            // Placeholder this is IE 7 and higher
+            $('input, textarea').placeholder();
+        },
+
+        FormValidator: {
+
+            init: function(){
+                $('#submitBtn, .submit-span').formValidator({ scope: '#registerForm' });
+            }
+
+        }
+
+    }
+
+};
+
+// When DOM Ready
+$(document).ready(function(){
+    PF.init();
+});
+
