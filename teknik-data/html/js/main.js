2f20304414ae289c316c389a1c35f0bc89f665a8
Volkan <kozaxs@windowslive.com>
2014-01-15 14:08:03 +0200

    lokasyon testi

--- teknik-data/html/js/main.js
+++ teknik-data/html/js/main.js
@@ -51,7 +51,7 @@ TD = {
                 });
             }
         },
-        //Deneme deneme   SasASasasdasda
+        //Lokasyon Testi
         CustomCheckBox: {
             init: function(){
                 $(document).on("click", ".custom-checkbox > input[type='checkbox']", function () { $(this).siblings('span.ck').toggleClass('active'); });
